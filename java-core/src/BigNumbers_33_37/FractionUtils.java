2e4188816925e808ebf9906126249a9c5c663e56
Artem Burlaka <guestartem@gmail.com>
2016-01-01 16:08:09 +0200

    Lab33

---
+++ java-core/src/BigNumbers_33_37/FractionUtils.java
@@ -0,0 +1,7 @@
+package BigNumbers_33_37;
+
+/**
+ * Created by Artem on 31.12.2015.
+ */
+public class FractionUtils {
+}
---
+++ java-core/src/BigNumbers_33_37/MainLab_33_37.java
@@ -0,0 +1,12 @@
+package BigNumbers_33_37;
+
+
+/**
+ * Created by Artem on 31.12.2015.
+ */
+public class MainLab_33_37 {
+    public static void main(String[] args) {
+
+    }
+
+}
