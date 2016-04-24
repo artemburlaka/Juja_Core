ead01ed5ae402e134310a706e78e80db0ddf723a
Artem Burlaka <guestartem@gmail.com>
2016-04-24 20:45:29 +0300

    lab_40 not end

---
+++ java-core/src/Exceptions_38_39/TestForLab39.java
@@ -0,0 +1,83 @@
+package Exceptions_38_39;
+
+/**
+ * Created by Artem on 20.03.2016.
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestForLab39{
+    public static void main(String[] args) {
+        //prepare
+        final List<String> actualHistoryCall = new ArrayList<String>();
+        final List<String> expectedExceptionTextAndOrder = new ArrayList<String>();
+        expectedExceptionTextAndOrder.add("Error factoryB.createB");
+        expectedExceptionTextAndOrder.add("Error closeA");
+        List<String> expectedHistoryCall = Arrays.asList("factoryA.createA", "factoryB.createB", "A.close");
+
+        final AutoCloseable resourceA = new AutoCloseable() {
+            @Override
+            public void close() throws Exception {
+                actualHistoryCall.add("A.close");
+                throw new Error(expectedExceptionTextAndOrder.get(1));
+            }
+        };
+
+        final AutoCloseable resourceB = new AutoCloseable() {
+            @Override
+            public void close() throws Exception {
+                actualHistoryCall.add("B.close");
+            }
+        };
+
+        AutoCloseableFactory factoryA = new AutoCloseableFactory() {
+            @Override
+            public AutoCloseable create() throws Throwable {
+                actualHistoryCall.add("factoryA.createA");
+                return resourceA;
+            }
+
+
+        };
+
+        AutoCloseableFactory factoryB = new AutoCloseableFactory() {
+            @Override
+            public AutoCloseable create() {
+                actualHistoryCall.add("factoryB.createB");
+                throw new Error(expectedExceptionTextAndOrder.get(0));
+            }
+
+
+        };
+
+        TryBody tryBody = new TryBody() {
+            @Override
+            public void runBody() {
+                actualHistoryCall.add("TryBody.runBody");
+                //NOP
+            }
+        };
+
+        //call and  check
+        try {
+            Lab39.twoResource(factoryA, factoryB, tryBody);
+        } catch (Throwable e) {
+
+            if (!expectedExceptionTextAndOrder.get(0).equals(e.getMessage()))
+                throw new AssertionError("Not correct main exception");
+
+            if (e.getSuppressed().length == 0)
+                throw new AssertionError("Should be suppressed exceptions  ");
+
+            if(!expectedExceptionTextAndOrder.get(1).equals(e.getSuppressed()[0].getMessage()))
+                throw new AssertionError("Not correct suppressed exception should to be "+expectedExceptionTextAndOrder.get(1) +" but found " + e.getSuppressed()[0].getMessage());
+        }
+
+
+        if (!actualHistoryCall.equals(expectedHistoryCall))
+            throw new AssertionError("Not correct order call should be " + expectedHistoryCall.toString() + " but found " + actualHistoryCall.toString());
+
+        System.out.print("OK");
+    }
+}
